0d0bf5809326b497c2312153e583cf7e8fd2db36
Jansen52x <yongray.teo.2022@scis.smu.edu.sg>
2023-09-05 14:04:25 +0800

    Setting up of BE

--- BE/Scripts/activate.bat
+++ BE/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\weilo\Documents\GitHub\WADII_Project\BE
+set VIRTUAL_ENV=Z:/wad2/WADII_Project/BE
 
 if not defined PROMPT set PROMPT=$P$G
 
