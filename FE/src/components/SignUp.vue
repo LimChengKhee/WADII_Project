dfe5de91576d1265fcc4c0a19e0cc65a665040ff
Lim Cheng Khee <cklim.2022@scis.smu.edu.sg>
2023-09-10 14:24:15 +0800

    Register Page

---
+++ FE/src/components/SignUp.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1> Placeholder for LOGO </h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name"/>
+        <br>
+        <input type="text" v-model="email" placeholder="Enter Email"/>
+        <br>
+        <input type="password" v-model="password" placeholder="Enter Password"/>
+        <br>
+        <button v-on:click="signUp">Sign Up</button>
+        <p>
+            <router-link to="/login">Login</router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{
+    name:"SignUp",
+    data(){
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+}
+</script>
--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -1,16 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Login from "../components/Login.vue";
+import SignUp from "../components/SignUp.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/login",
+    name: "login",
+    component: Login,
   },
+  {
+    path: "/sign-up",
+    name: "SignUp",
+    component: SignUp,
+  }
 ];
 
+
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 
