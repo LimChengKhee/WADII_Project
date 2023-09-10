aa653f4c0b9828ecac712b8e63d66759a21b7ca5
Lim Cheng Khee <cklim.2022@scis.smu.edu.sg>
2023-09-10 15:03:26 +0800

    Register and Login Updates

---
+++ FE/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Login Page</h1>
+        <form>
+            <div class="register">
+                <input type="text" v-model="email" placeholder="Enter Email" />
+                <br />
+                <input type="password" v-model="password" placeholder="Enter Password" />
+                <br />
+                <button>Login</button>
+            </div>
+        </form>
+        <p>
+            <router-link to="/sign-up">Sign Up</router-link>
+        </p>
+    </div>
+</template>
+  
+<script>
+    export default{
+        name: 'Login',
+    };
+</script>
---
+++ FE/src/views/SignUp.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Sign Up</h1>
+        <img class = "logo" src="../img/logo.jpeg">
+        <form>
+            <div class="register">
+                <input type="text" v-model="name" placeholder="Enter Name" />
+                <br />
+                <input type="text" v-model="email" placeholder="Enter Email" />
+                <br />
+                <input type="password" v-model="password" placeholder="Enter Password" />
+                <br />
+                <button type="submit" v-on:click="signUp">Sign Up</button>
+            </div>
+        </form>
+        <p>
+            <router-link to="/login">Login</router-link>
+        </p>
+    </div>
+</template>
+  
+
+<script>
+import axios from 'axios'
+export default {
+    name: "SignUp",
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+}
+</script>
+<style>
+
+</style>
