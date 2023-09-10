ee0cc987bff48b8a3ea863fc0cc83393a805952d
Lim Cheng Khee <cklim.2022@scis.smu.edu.sg>
2023-09-10 15:16:32 +0800

    Update main.js

--- FE/src/main.js
+++ FE/src/main.js
@@ -1,6 +1,11 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+const app = createApp(App)
 
-createApp(App).use(store).use(router).mount('#app')
+app.use(router)
+
+app.mount('#app')
