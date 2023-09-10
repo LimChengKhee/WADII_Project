a483c81b45eb8cd5055fda2893dc64c36d898bc3
Lim Cheng Khee <cklim.2022@scis.smu.edu.sg>
2023-09-10 15:21:41 +0800

    CSS Updates

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
@@ -13,24 +14,6 @@ export default {
   name: "App",
 };
 </script>
-
-<style lang="scss">
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  font-family: "Raleway", sans-serif;
-  font-weight: 400;
-}
-
-.app {
-  min-height: 100vh;
-  position: relative;
-  background-color: #f1f1f1;
-}
-.container {
-  padding: 0 20px;
-  max-width: 1140px;
-  margin: 0 auto;
-}
+<style>
+  @import '..\FE\src\assets\login.css';
 </style>
