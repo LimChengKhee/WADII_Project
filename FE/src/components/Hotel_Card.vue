b2b07b87ff3a8b43428b29d0b238cef54cab6a57
Wei Long! <weilong000827@gmail.com>
2023-09-12 02:00:09 +0800

    rough draft of hotels page.

---
+++ FE/src/components/Hotel_Card.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container mt-4 mb-2">
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col-10">
+
+        <div class="card mb-3" style="width: 75vw; height: 50vh">
+          <div class="row g-0">
+            <div class="col-md-3">
+              <img src="../assets/mbs.jpg" class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body">
+                <h5 class="card-title">PARKROYAL Service Hotel Kuala Lumpur</h5>
+                <div class="features">
+                    <span class="badge rounded-pill text-bg-secondary">2.2km from City Centre</span><br/>
+                    <div class="container mt-5">
+                        <div class="row mt-3 mb-4">
+                            <img src="../icons/bathroom.png" alt="" style="width:45px;"><div class="col-3 ps-0">
+                                Bathroom</div>
+                            <img src="../icons/bathroom.png" alt="" style="width:45px;"><div class="col-3 ps-0">Bathroom</div>
+                        </div>
+
+                        <div class="row mt-3 mb-4">
+                            <img src="../icons/bathroom.png" alt="" style="width:45px;"><div class="col-3 ps-0">
+                                Bathroom</div>
+                            <img src="../icons/bathroom.png" alt="" style="width:45px;"><div class="col-3 ps-0">Bathroom</div>
+                        </div>
+
+                        <div class="row mt-3 mb-4">
+                            <img src="../icons/bathroom.png" alt="" style="width:45px;"><div class="col-3 ps-0">
+                                Bathroom</div>
+                            <img src="../icons/bathroom.png" alt="" style="width:45px;"><div class="col-3 ps-0">Bathroom</div>
+                        </div>
+
+
+                    </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-2 pe-4">
+                <div class="container">
+                    <div class="row mt-5">
+                        <!--Ratings / achievements-->
+                        Ratings / Achievements
+                    </div>
+                    <div class="row mt-3 mb-5">
+                        <!--Price-->
+                        Price
+                    </div>
+                    <div class="row mt-3 mb-5">
+                        <!--Rating-->
+                        Rating
+                    </div>
+                    <div class="row mt-3">
+                        <button type="button" class="btn btn-warning">More Info</button>
+                    </div>
+
+                </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+      <div class="col-1"></div>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'HotelCard',
+  components: {},
+  data() {
+    // local repository of information
+    return {}
+  },
+  computed: {
+    // computed
+  },
+
+  // start of lifecycle
+  async mounted() {},
+
+  methods: {
+    // methods defined by ourselves
+    example() {}
+  }
+}
+</script>
+
+<style scoped></style>
--- FE/src/views/Hotel_Flights.vue
+++ FE/src/views/Hotel_Flights.vue
@@ -1,80 +1,158 @@
 <template>
-  <!--Sidebar-->
-  <div class="parent_container">  
-    <div class="header" style="text-align: center; font-size:60px;">
-      <p>Hotels & Flights</p>
-    </div>
-    <div class="flight_container" style="display:flex;justify-content: center;">
-      <div class="card" style="width: 50rem;">
-        <div class="card-body">
-            <div class="row-of-buttons">
-              <div class="dropdown">
-            <button class="btn btn dropdown-toggle ps-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Round Trip
+
+  <div class="parent-container">
+    <div class="header">
+      <div class="container-fluid mt-5 ms-5 mb-3">
+        <div class="row">
+          <div class="col">
+            <h2 style="text-align: center;">Search accomodation</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p style="text-align: center;">
+              Search for accomodation deals for {} in {}
+            </p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="container-fluid ms-5 mb-3">
+        <div class="row">
+          <div class="col-4"></div>
+          <div class="col-2 me-3">
+            <!--inputted value in it-->
+            <input type="text" value="Kuala Lumpur">
+          </div>
+          <div class="col-3">
+            <input type="date" value="13th September 2023">
+          </div>
+          <div class="col-3">
+            <!-- human logos in it -->
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid filter-button">
+        <div class="row ms-4">
+          <div class="col-3"></div>
+          <!--PRICE FILTER-->
+          <div class="col-1 ms-4" style="padding-left: 0px;">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Price  
             </button>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Round Trip</a></li>
-              <li><a class="dropdown-item" href="#">One-Way</a></li>
-              <li><a class="dropdown-item" href="#">Multi-City</a></li>
-            </ul>
+            <!--slider-->
           </div>
 
-          <div class="row row_of_inputs">
-            <div class="col-3">
-              <label for="location" class="form-label">Location</label>
-              <input type="text" class="form-control" id="location" placeholder="Location">
+          <!--AMENITIES FILTER-->
+          <div class="col-1 mx-1" style="padding-left: 0px;">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Amenities
+            </button>
+
+            <!--SHOWER CHECKBOX-->
+            <div class="dropdown-menu ps-2">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="showerCheck">
+                <label class="form-check-label" for="showerCheck">
+                  Shower
+                </label>
+              </div>
+
+            <!--BATHROOM CHECKBOX-->
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="bathroomCheck">
+                <label class="form-check-label" for="bathroomCheck">
+                  Bathroom
+                </label>
+              </div>
+
+              <!--CASINO CHECKBOX-->
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="casinoCheck">
+                <label class="form-check-label" for="casinoCheck">
+                  Casino
+                </label>
+              </div>
             </div>
-            <div class="col-3">
-              <label for="location" class="form-label">Location</label>
-              <input type="text" class="form-control" id="location" placeholder="Location">
+
+          </div>
+          
+          <div class="col-1 mx-4" style="padding-left: 0px;">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Star Rating
+            </button>
+            <div class="dropdown-menu ps-2">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="one_star">
+                <label class="form-check-label" for="one_star">
+                  1 Star
+                </label>
+              </div>
+
+            <!--BATHROOM CHECKBOX-->
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="two_star">
+                <label class="form-check-label" for="two_star">
+                  2 Star
+                </label>
+              </div>
+
+              <!--CASINO CHECKBOX-->
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="three_star">
+                <label class="form-check-label" for="three_star">
+                  3 Star
+                </label>
+              </div>
+
+                <!--CASINO CHECKBOX-->
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="four_star">
+                <label class="form-check-label" for="four_star">
+                  4 Star
+                </label>
+              </div>
+
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="five_star">
+                <label class="form-check-label" for="five_star">
+                  5 Star
+                </label>
+              </div>
             </div>
-            <div class="col-6">
-              <label for="location" class="form-label">Location</label>
-              <input type="text" class="form-control" id="location" placeholder="Location">
-            </div>
+          </div>
+          
+          <div class="col-1 mx-4" style="padding-left: 0px;">
+            <input type="text" class="search">
+              <!--Search text box-->
+          
+          </div>
+
+          <!--Pending search button-->
+
         </div>
       </div>
-    </div>
-      
-    
+      </div>
+      <div class="card_section">
+        <HotelCard/>
+        <HotelCard/>
+        <HotelCard/>
+        <HotelCard/>
+        <HotelCard/>
+      </div>
+
   </div>
-    </div>
-
-    <div class="hotel_container" style="display:flex;justify-content: center; margin-top: 50px;">
-      <div class="card" style="width: 50rem;">
-        <div class="card-body">
-
-          <div class="row row_of_inputs">
-            <div class="col-3">
-              <label for="hotel" class="form-label">Hotel</label>
-              <input type="text" class="form-control" id="hotel" placeholder="Hotel">
-            </div>
-            <div class="col-3">
-              <label for="location" class="form-label">Duration</label>
-              <input type="text" class="form-control" id="duration" placeholder="Duration">
-            </div>
-            <div class="col-6">
-              <label for="people" class="form-label">People</label>
-              <input type="text" class="form-control" id="people" placeholder="Number of people">
-            </div>
-        </div>
-      </div>
-    </div>
-      
-
-    </div>
-
-   </div>
+  
 </template>
   <script>
-  //  import statements
-  // import example from '@/utils/string_formatter'
-  import Single
+  import HotelCard from '../components/Hotel_Card.vue';
+
   export default {
     name: 'Hotel_Flights',
     components: { 
       // importing components from other places
-
+      HotelCard
     },
     data () {
       // local repository of information
@@ -100,6 +178,8 @@
   </script>
   
   <style scoped>
-  
+    .banner {
+      background-color: blue;
+    }
   </style>
   
