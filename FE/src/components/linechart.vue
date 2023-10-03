6bb21b755732dae9dba46afbf151696e5524d0fc
Lim Cheng Khee <cklim.2022@scis.smu.edu.sg>
2023-10-03 20:35:24 +0800

    Update Dashboard

--- FE/src/components/linechart.vue
+++ FE/src/components/linechart.vue
@@ -1,20 +1,28 @@
 <template>
     <div>
-        <h2>Test</h2>
+      <h2>Line Chart</h2>
+      <div id="chartContainer">
         <svg></svg>
+      </div>
     </div>
-</template>
-<script>
-import * as d3 from "d3";
-export default {
+  </template>
+  
+  <script>
+  import * as d3 from "d3";
+  
+  export default {
     name: 'Linechart',
     data() {
-        return {};
+      return {};
     },
     mounted() {
-        const width = 800;
-        const height = 500;
-        const data = [{ date: "24-Apr-07", amount: 93.24 },
+      const container = d3.select("#chartContainer");
+      const containerWidth = container.node().getBoundingClientRect().width;
+      const margin = { top: 20, right: 20, bottom: 40, left: 40 };
+      const width = containerWidth - margin.left - margin.right;
+      const height = 500 - margin.top - margin.bottom;
+      const data = [
+      { date: "24-Apr-07", amount: 93.24 },
         { date: "25-Apr-07", amount: 95.35 },
         { date: "26-Apr-07", amount: 98.84 },
         { date: "27-Apr-07", amount: 99.92 },
@@ -26,66 +34,63 @@ export default {
         { date: "7-May-07", amount: 103.92 },
         { date: "8-May-07", amount: 105.06 },
         { date: "9-May-07", amount: 106.88 },
-        { date: "10-May-07", amount: 107.34 },];
-
-        const svg = d3.select("svg").attr("width", width).attr("height", height);
-        const g = svg.append("g");
-
-        //2. Parse the dates
-        const parseTime = d3.timeParse("%d-%b-%y");
-
-        //3. Creating the Chart Axes
-        const x = d3
-            .scaleTime()
-            .domain(
-                d3.extent(data, function (d) {
-                    return parseTime(d.date);
-                })
-            )
-            .rangeRound([0, width]);
-
-        const y = d3
-            .scaleLinear()
-            .domain(
-                d3.extent(data, function (d) {
-                    return d.amount;
-                })
-            )
-            .rangeRound([height, 0]);
-
-        //4. Creating a Line
-        const line = d3
-            .line()
-            .x(function (d) {
-                return x(parseTime(d.date));
-            })
-            .y(function (d) {
-                return y(d.amount);
-            });
-
-        //5. Appending the Axes to the Chart
-        g.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        g.append("g")
-            .call(d3.axisLeft(y))
-            .append("text")
-            .attr("fill", "#000")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Price ($)");
-
-        //6. Appending a path to the Chart
-        g.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 1.5)
-            .attr("d", line);
+        { date: "10-May-07", amount: 107.34 },
+      ];
+  
+      const svg = d3.select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+        
+  
+      const g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+      const parseTime = d3.timeParse("%d-%b-%y");
+  
+      const x = d3
+        .scaleTime()
+        .domain(d3.extent(data, d => parseTime(d.date)))
+        .rangeRound([0, width]);
+  
+      const y = d3
+        .scaleLinear()
+        .domain(d3.extent(data, d => d.amount))
+        .rangeRound([height, 0]);
+  
+      const line = d3
+        .line()
+        .x(d => x(parseTime(d.date)))
+        .y(d => y(d.amount));
+  
+      g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-width", 3)
+        .attr("d", line);
+  
+      g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+  
+      // Style x-axis labels
+      svg.selectAll(".tick text")
+        .attr("fill", "black")
+        .attr("text-anchor", "middle")
+        .attr("dy", "1em")
     },
-}
+  };
+  </script>
+  
+  <style scoped>
+  #chartContainer {
+    width: 100%; /* Ensure the container takes the full width */
+  }
 
-</script>
+  *{
+    background-color: beige;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    font-weight: bold;
+  }
+  </style>
+  
--- FE/src/views/dashboard.vue
+++ FE/src/views/dashboard.vue
@@ -1,96 +1,137 @@
 <template>
     <div class="container-fluid">
-        <div class="row px-0">
-            <div class="col-3 p-5 sidebar">
-                <img class="shadow-4-strong img-circle rounded-circle custom-width my-3" alt="profileImg"
-                    src="../img/steve.jpg" />
-                <div class="profileInfo">
-                    <h5>{{ profileInfo[0].Name }}</h5>
-                    <p>{{ profileInfo[0].Birthday }}</p>
-                    <p>{{ profileInfo[0].Gender }}</p>
-                </div>
-            </div>
-            <div class="col-9 main-content">
-                <!-- Your main content here -->
-                <div class="row keyStats">
-                    <div class="col-3">
-                        <Linechart/>
-                    </div>
-                    <div class="col-3">
-
-                    </div>
-                    <div class="col-3">
-
-                    </div>
-                    <div class="col-3">
-
-                    </div>
-                </div>
-                <div class="row priChart">
-                    <Linechart/>
-                </div>
-                <div class="row secCharts">
-                    <div class="col-4">
-
-                    </div>
-                    <div class="col-4">
-
-                    </div>
-                    <div class="col-4">
-
-                    </div>
-                </div>
-            </div>
+      <div class="row px-0">
+        <div class="col-lg-3 col-md-4 col-12 p-5 sidebar">
+          <img class="shadow-4-strong img-circle rounded-circle custom-width my-3" alt="profileImg" src="../img/steve.jpg" />
+          <div class="profileInfo">
+            <h5>{{ profileInfo[0].Name }}</h5>
+            <p>{{ profileInfo[0].Birthday }}</p>
+          </div>
         </div>
+        <div class="col-lg-9 col-md-8 col-12 main-content">
+          <div class="row keyStats">
+            <div class="col-lg-3 col-md-6 col-12">
+              <!-- Content for the first key stat column -->
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+              <!-- Content for the second key stat column -->
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+              <!-- Content for the third key stat column -->
+            </div>
+            <div class="col-lg-3 col-md-6 col-12">
+              <!-- Content for the fourth key stat column -->
+            </div>
+          </div>
+          <div class="row priChart">
+            <div class="col-lg-12 px-0">
+              <Linechart :filteredData="filteredData" />
+            </div>
+          </div>
+          <div class="row secCharts">
+            <div class="col-lg-4 col-md-6 col-12">
+              <Piechart :filteredData="filteredData" />
+            </div>
+            <div class="col-lg-4 col-md-6 col-12">
+              <Piechart :filteredData="filteredData" />
+            </div>
+            <div class="col-lg-4 col-12">
+              <Piechart :filteredData="filteredData" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
   
-<script>
-import Linechart from "../components/linechart.vue";
-
-export default {
+  <script>
+  import * as d3 from "d3";
+  import crossfilter from "crossfilter"; // Use crossfilter2 for ES6 compatibility
+  
+  import Linechart from "../components/linechart.vue";
+  import Piechart from "../components/piechart.vue";
+  
+  export default {
     name: 'Dashboard',
-    components:{
-    Linechart,
-  },
-    data() {
-        return {
-            profileInfo: [
-                {
-                    id: 1,
-                    Name: "Mike Tyson",
-                    Birthday: "01 Sep 2000",
-                    Gender: "M",
-                },
-            ],
-        };
+    components: {
+      Linechart,
+      Piechart,
     },
-};
-</script>
+    data() {
+      return {
+        profileInfo: [
+          {
+            id: 1,
+            Name: "Mike Tyson",
+            Birthday: "01 Sep 2000",
+          },
+        ],
+        data: [
+          { date: "24-Apr-07", amount: 93.24 },
+          { date: "25-Apr-07", amount: 95.35 },
+          { date: "26-Apr-07", amount: 98.84 },
+          // ... more data ...
+        ],
+        crossfilterData: null,
+        filteredData: [],
+      };
+    },
+    mounted() {
+      // Call a function to set up Crossfilter when the component is mounted
+      this.setupCrossfilter();
+    },
+    methods: {
+      setupCrossfilter() {
+        // Create a Crossfilter instance
+        this.crossfilterData = crossfilter(this.data);
   
-
-<style>
-.img-circle {
+        // Define dimensions based on the data properties you want to filter on
+        const dateDimension = this.crossfilterData.dimension((d) => d.date);
+  
+        // Store the dimension in the component's data
+        this.dateDimension = dateDimension;
+  
+        // Apply an initial filter
+        this.dateDimension.filterRange(["24-Apr-07", "27-Apr-07"]);
+  
+        // Initialize the filteredData
+        this.updateFilteredData();
+  
+        // Listen for changes to the Crossfilter data
+        this.crossfilterData.on("filtered", () => {
+          this.updateFilteredData();
+        });
+      },
+      updateFilteredData() {
+        // Update the filteredData based on the current Crossfilter state
+        this.filteredData = this.dateDimension.top(Infinity);
+      },
+    },
+  };
+  </script>
+  
+  
+  <style>
+  .img-circle {
     height: 200px;
     border: 2px solid black;
-}
-
-div.row img.rounded-circle.custom-width {
+  }
+  
+  div.row img.rounded-circle.custom-width {
     width: auto;
-}
-
-.sidebar {
+  }
+  
+  .sidebar {
     background-color: black;
     text-align: center;
-}
-
-.main-content {
+  }
+  
+  .main-content {
     background-color: grey;
-}
-
-.profileInfo {
+  }
+  
+  .profileInfo {
     color: white;
-
-}
-</style>
-
+  }
+  </style>
+  
