29914848f42ea56310d00c02d868d81aaef3e51f
Wei Long! <weilong000827@gmail.com>
2023-10-21 12:27:11 +0800

    final side bar addition

--- FE/src/router/index.js
+++ FE/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Hotel from '../views/Hotel.vue'
 import Home from '../views/Home.vue'
 import Hotel_Page from '../views/Hotel_Page.vue'
+import SideBar from '../components/SideBar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/hotel/:name',
       name: 'Specific',
       component: Hotel_Page
+    },
+    {
+      path: '/hotel/sidebar',
+      name: 'SideBar',
+      component: SideBar
     }
   ]
 })
--- FE/src/views/Hotel.vue
+++ FE/src/views/Hotel.vue
@@ -1,81 +1,91 @@
 <template>
   <div class="parent-container">
-    <div class="header">
-      <div class="container-fluid mt-5 ms-5 mb-3">
-        <div class="row">
-          <div class="col">
-            <h2 style="text-align: center">Search accomodation</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col">
-            <p style="text-align: center">Search for accomodation deals in {}</p>
-          </div>
-        </div>
+    <div class="row">
+      <div class="col-2">
+        <Side_Bar />
       </div>
-
-      <div class="container">
-        <div class="row">
-          <div class="col-1"></div>
-          <div class="col-3">
-            <input v-model="user_search" type="text">
-            <input type="button" value="Search" @click="searchHotelName()">
-          </div>
-          <div class="col-5"></div>
-          <div class="col-1">
-            <span>
-              <div class="dropdown" style="width: 100%">
-                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                  aria-expanded="false">
-                  Sorting Criteria
-                </button>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Review Rating
-                      <span v-if="review_sort" @click="sort_review()"><img src="../assets/sort-up.png" style="width:10px"
-                          alt=""></span>
-
-                      <span @click="sort_review()" v-else><img src="../assets/caret-down.png" style="width:10px"
-                          alt=""></span>
-                    </a></li>
-                  <li><a class="dropdown-item" href="#">Distance
-                      <span v-if="distance_sort" @click="sort_distance()"><img src="../assets/sort-up.png"
-                          style="width:10px" alt=""></span>
-
-                      <span v-else @click="sort_distance()"><img src="../assets/caret-down.png" style="width:10px"
-                          alt=""></span>
-
-                    </a></li>
-                </ul>
+      <div class="col-10 ">
+        <div class="header">
+          <div class="container-fluid mt-5 ms-5 mb-3">
+            <div class="row">
+              <div class="col">
+                <h2 style="text-align: center">Search accomodation</h2>
               </div>
-
-            </span>
+            </div>
+            <div class="row">
+              <div class="col">
+                <p style="text-align: center">Search for accomodation deals in {}</p>
+              </div>
+            </div>
           </div>
+
+          <div class="container">
+            <div class="row">
+              <div class="col-1"></div>
+              <div class="col-3">
+                <input v-model="user_search" type="text">
+                <input type="button" value="Search" @click="searchHotelName()">
+              </div>
+              <div class="col-5"></div>
+              <div class="col-1">
+                <span>
+                  <div class="dropdown" style="width: 100%">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                      aria-expanded="false">
+                      Sorting Criteria
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Review Rating
+                          <span v-if="review_sort" @click="sort_review()"><img src="../assets/sort-up.png"
+                              style="width:10px" alt=""></span>
+
+                          <span @click="sort_review()" v-else><img src="../assets/caret-down.png" style="width:10px"
+                              alt=""></span>
+                        </a></li>
+                      <li><a class="dropdown-item" href="#">Distance
+                          <span v-if="distance_sort" @click="sort_distance()"><img src="../assets/sort-up.png"
+                              style="width:10px" alt=""></span>
+
+                          <span v-else @click="sort_distance()"><img src="../assets/caret-down.png" style="width:10px"
+                              alt=""></span>
+
+                        </a></li>
+                    </ul>
+                  </div>
+
+                </span>
+              </div>
+            </div>
+          </div>
+
         </div>
+        <div class="card_section" v-if="mounted && !search">
+          <HotelCard v-for="hotel in this.hotelsInCities" :hotel_name="hotel.hotel_name"
+            :distance_to_cc_formatted="hotel.distance_to_cc_formatted" :review_score="hotel.review_score"
+            :review_score_word="hotel.review_score_word" :number_of_reviews="hotel.review_nr"
+            :photo_url="hotel.max_1440_photo_url"
+            :price_per_night="hotel.composite_price_breakdown.gross_amount_per_night.value"
+            :currency="hotel.composite_price_breakdown.gross_amount_per_night.currency" :updated_object="hotel"
+            :district="hotel.district" :city="hotel.city"></HotelCard>
+        </div>
+        <div class="card_section" v-if="mounted && search">
+          <HotelCard v-for="hotel in this.sub_hotels" :hotel_name="hotel.hotel_name"
+            :distance_to_cc_formatted="hotel.distance_to_cc_formatted" :review_score="hotel.review_score"
+            :review_score_word="hotel.review_score_word" :number_of_reviews="hotel.review_nr"
+            :photo_url="hotel.max_1440_photo_url"
+            :price_per_night="hotel.composite_price_breakdown.gross_amount_per_night.value"
+            :currency="hotel.composite_price_breakdown.gross_amount_per_night.currency" :updated_object="hotel"
+            :district="hotel.district" :city="hotel.city"></HotelCard>
+        </div>
+        <div class="spinner_section d-flex flex-column" v-if="!mounted">
+          <div class="spinner-border mt-5" style="margin: 0 auto;height:200px;width:200px;" role="status">
+          </div>
+          <p class="mt-2" style="text-align: center;">Loading...Please give us a second</p>
+        </div>
+
+
       </div>
 
-    </div>
-    <div class="card_section" v-if="mounted && !search">
-      <HotelCard v-for="hotel in this.hotelsInCities" :hotel_name="hotel.hotel_name"
-        :distance_to_cc_formatted="hotel.distance_to_cc_formatted" :review_score="hotel.review_score"
-        :review_score_word="hotel.review_score_word" :number_of_reviews="hotel.review_nr"
-        :photo_url="hotel.max_1440_photo_url"
-        :price_per_night="hotel.composite_price_breakdown.gross_amount_per_night.value"
-        :currency="hotel.composite_price_breakdown.gross_amount_per_night.currency" :updated_object="hotel"
-        :district="hotel.district" :city="hotel.city"></HotelCard>
-    </div>
-    <div class="card_section" v-if="mounted && search">
-      <HotelCard v-for="hotel in this.sub_hotels" :hotel_name="hotel.hotel_name"
-        :distance_to_cc_formatted="hotel.distance_to_cc_formatted" :review_score="hotel.review_score"
-        :review_score_word="hotel.review_score_word" :number_of_reviews="hotel.review_nr"
-        :photo_url="hotel.max_1440_photo_url"
-        :price_per_night="hotel.composite_price_breakdown.gross_amount_per_night.value"
-        :currency="hotel.composite_price_breakdown.gross_amount_per_night.currency" :updated_object="hotel"
-        :district="hotel.district" :city="hotel.city"></HotelCard>
-    </div>
-    <div class="spinner_section d-flex flex-column" v-if="!mounted">
-      <div class="spinner-border mt-5" style="margin: 0 auto;height:200px;width:200px;" role="status">
-      </div>
-      <p class="mt-2" style="text-align: center;">Loading...Please give us a second</p>
     </div>
   </div>
 </template>
@@ -83,11 +93,13 @@
 import HotelCard from '../components/Hotel_Card.vue'
 import axios from 'axios'
 import flushPromises from 'flush-promises'
+import Side_Bar from "../components/SideBar.vue"
 
 export default {
   name: 'Hotel',
   components: {
     // importing components from other places
+    Side_Bar,
     HotelCard
   },
   data() {
@@ -121,26 +133,26 @@ export default {
   methods: {
     sort_review(direction_to_go) {
       this.review_sort = !this.review_sort;
-      if(!this.review_sort){
+      if (!this.review_sort) {
         this.hotelsInCities = this.hotelsInCities.sort((a, b) => {
-        return Number(a["review_score"]) - Number(b["review_score"]) 
-      })
-      } else{
+          return Number(a["review_score"]) - Number(b["review_score"])
+        })
+      } else {
         this.hotelsInCities = this.hotelsInCities.sort((a, b) => {
-          return  Number(b["review_score"])  - Number(a["review_score"])
-      })
+          return Number(b["review_score"]) - Number(a["review_score"])
+        })
       }
     },
     sort_distance() {
       this.distance_sort = !this.distance_sort;
-      if(!this.distance_sort){
+      if (!this.distance_sort) {
         this.hotelsInCities = this.hotelsInCities.sort((a, b) => {
-        return Number(b["distance_to_cc"]) - Number(a["distance_to_cc"])  
-      })
-      } else{
+          return Number(b["distance_to_cc"]) - Number(a["distance_to_cc"])
+        })
+      } else {
         this.hotelsInCities = this.hotelsInCities.sort((a, b) => {
-        return Number(a["distance_to_cc"] - Number(b["distance_to_cc"]))  
-      })
+          return Number(a["distance_to_cc"] - Number(b["distance_to_cc"]))
+        })
       }
     },
     searchHotelName() {
@@ -182,41 +194,41 @@ export default {
       //   }
       // };
 
-      const options = {
-        method: 'GET',
-        url: 'https://booking-com.p.rapidapi.com/v1/hotels/search',
-        params: {
-          checkin_date: '2023-11-27',
-          dest_type: 'city',
-          units: 'metric',
-          checkout_date: '2023-11-28',
-          adults_number: '2',
-          order_by: 'popularity',
-          dest_id: '-553173',
-          filter_by_currency: 'AED',
-          locale: 'en-gb',
-          room_number: '1',
-          children_number: '2',
-          children_ages: '5,0',
-          categories_filter_ids: 'class::2,class::4,free_cancellation::1',
-          page_number: '0',
-          include_adjacency: 'true'
-        },
-        headers: {
-          'X-RapidAPI-Key': '1b483ecf55mshf8532b1de0b460ep15ab30jsnbb634c85aadd',
-          'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
-        }
-      };
+      // const options = {
+      //   method: 'GET',
+      //   url: 'https://booking-com.p.rapidapi.com/v1/hotels/search',
+      //   params: {
+      //     checkin_date: '2023-11-27',
+      //     dest_type: 'city',
+      //     units: 'metric',
+      //     checkout_date: '2023-11-28',
+      //     adults_number: '2',
+      //     order_by: 'popularity',
+      //     dest_id: '-553173',
+      //     filter_by_currency: 'AED',
+      //     locale: 'en-gb',
+      //     room_number: '1',
+      //     children_number: '2',
+      //     children_ages: '5,0',
+      //     categories_filter_ids: 'class::2,class::4,free_cancellation::1',
+      //     page_number: '0',
+      //     include_adjacency: 'true'
+      //   },
+      //   headers: {
+      //     'X-RapidAPI-Key': '1b483ecf55mshf8532b1de0b460ep15ab30jsnbb634c85aadd',
+      //     'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+      //   }
+      // };
 
-      try {
-        const response = await axios.request(options)
-        console.log(response);
-        this.hotelsInCities = response.data.result
-      } catch (error) {
-        console.error(error);
-      }
+      // try {
+      //   const response = await axios.request(options)
+      //   console.log(response);
+      //   this.hotelsInCities = response.data.result
+      // } catch (error) {
+      //   console.error(error);
+      // }
 
-      await flushPromises()
+      // await flushPromises()
 
       for (let hotel of this.hotelsInCities) {
         let hotel_id = hotel.hotel_id
@@ -265,4 +277,5 @@ export default {
 
 #search_input {
   position: absolute;
-}</style>
+}
+</style>
